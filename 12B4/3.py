85595b808d976297c997b5027b3aa043aa37913f
Chu Minh <chungocquangminh@gmail.com>
2024-09-10 23:12:24 +0700

    123

--- 12B4/3.py
+++ 12B4/3.py
@@ -1,3 +1,4 @@
+import time
 def binary_search(value, array):
     left, right = 0, len(array) - 1
     while left <= right:
@@ -10,4 +11,16 @@ def binary_search(value, array):
             right = mid - 1
     return -1
 
-binary_search(12, [2, 4, 6, 8, 10, 12, 14, 16, 18, 20])
+test_list = [9, 7, 5, 3, 1, 2, 4, 6, 8]
+
+trials = 1000  
+start_time = time.time()
+
+for _ in range(trials):
+    binary_search(test_list.copy())
+
+end_time = time.time()
+
+average_time = (end_time - start_time) / trials
+
+print("Time Taken for {} trials: {:.6f} seconds".format(trials, average_time))
--- 12B4/4.py
+++ 12B4/4.py
@@ -1,3 +1,5 @@
+import time
+
 def insertion_sort(array):
     n = len(array)
     for i in range(1, n):
@@ -7,4 +9,20 @@ def insertion_sort(array):
             array[j + 1] = array[j]
             j -= 1
         array[j + 1] = key
+    return array
 
+test_list = [9, 7, 5, 3, 1, 2, 4, 6, 8]
+
+trials = 100000
+start_time = time.time()
+
+for _ in range(trials):
+    insertion_sort(test_list.copy())
+
+end_time = time.time()
+
+# Calculate the average time taken
+average_time = (end_time - start_time) / trials
+
+# Print the average time taken
+print("Time Taken for {} trials: {:.6f} seconds".format(trials, average_time))
